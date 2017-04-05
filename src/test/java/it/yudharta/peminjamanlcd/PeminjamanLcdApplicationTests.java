25b47be811b79075c46962586d7ab7bc1de4ed39
nufail89 <nuvailmizzan@gmail.com>
2017-04-05 12:05:51 +0700

    Creating Project
    Perubahan & penambahan file:
    1. .gitignore -> sesuai dengan pembuatan project pertama di Framework
    Spring.
    2. pom.xml menambah dependency starter-web

---
+++ src/test/java/it/yudharta/peminjamanlcd/PeminjamanLcdApplicationTests.java
@@ -0,0 +1,16 @@
+package it.yudharta.peminjamanlcd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PeminjamanLcdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

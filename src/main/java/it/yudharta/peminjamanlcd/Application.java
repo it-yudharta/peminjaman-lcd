5993d4014f6ffe8645b95b551fd9d37fb2536104
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-04-05 22:10:27 +0700

    ganti nama aplikasi utama

--- src/main/java/it/yudharta/peminjamanlcd/Application.java
+++ src/main/java/it/yudharta/peminjamanlcd/Application.java
@@ -1,12 +1,12 @@
-package it.yudharta.peminjamanlcd.view;
+package it.yudharta.peminjamanlcd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PeminjamanLcdApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PeminjamanLcdApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }

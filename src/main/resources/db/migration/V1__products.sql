1eb40d4ebca37c272b4b306a28347f01616fd34a
Amiruddin Fadli <amiru.fadli@gmail.com>
2017-04-09 22:29:17 +0700

    Hapus ddl yang salah
    Ddl untuk tabel products dihapus karna yang benar adalah tabel Barang sesuai dengan kesepakatan.

--- src/main/resources/db/migration/V1__products.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `kode` varchar(25) DEFAULT NULL,
-  `nama` varchar(50) DEFAULT NULL,
-  `status` enum('ADA','KELUAR') DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;

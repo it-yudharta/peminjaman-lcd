9a34a269f8d201f6e0023a04c43de474bd8a52d0
lilismesesek <lilistrisnawati89@gmail.com>
2017-04-10 18:19:06 +0700

    resources template
    mengubah sedikit tampilan dari template sebenarnya.....:)

---
+++ src/main/resources/template.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Universitas Yudharta Pasuruan</title>
+<meta name="keywords" content="Red Blog Theme, Free CSS Templates" />
+<meta name="description" content="Red Blog Theme - Free CSS Templates by templatemo.com" />
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+
+<div id="templatemo_top_wrapper">
+	<div id="templatemo_top">
+    
+        <div id="templatemo_menu">
+                    
+            <ul>
+                <li><a href="index.html" class="current">Home</a></li>
+                <li><a href="portfolio.html">Portfolio</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="contact.html">Contact Us</a></li>
+            </ul>    	
+        
+        </div> <!-- end of templatemo_menu -->
+        
+        <div id="twitter">
+        	<a href="#">follow us <br />
+        	on twitter</a>
+        </div>
+        
+  </div>
+</div>
+
+<div id="templatemo_header_wrapper">
+	<div id="templatemo_header">
+    
+    	<div id="site_title">
+            <h1><a href="http://www.templatemo.com" target="_parent"><strong>Red Blog</strong><span>Free Blog Template in HTML CSS</span></a></h1>
+        </div>
+    
+    </div>
+</div>
+
+<div id="templatemo_main_wrapper">
+	<div id="templatemo_main">
+		<div id="templatemo_main_top">
+        
+        	<div id="templatemo_content">
+        
+    		<div class="post_section">
+            
+            	<div class="post_date">
+                	30<span>Nov</span>
+            	</div>
+<div class="post_content">
+                
+                    <h2><a href="blog_post.html">Peminjaman LCD</a></h2>
+
+                    <strong>Author:</strong> IT-YUDHARTA PASURUAN | <strong>Category:</strong> <a href="#">PSD</a>, <a href="#">Peminjaman LCD</a>
+                    
+                    <a href="http://www.templatemo.com/page/1" target="_parent"><img src="images/templatemo_image_01.jpg" alt="image" /></a>
+                    
+                    <p><a href="http://www.templatemo.com" target="_parent">Red Blog </a> is a free HTML/CSS layout from templatemo.com for everyone. There are total 5 pages included (blog, <a href="blog_post.html">full  post</a>, services, portfolio, contact) Quisque at ante sit amet erat laoreet fermentum. Quisque nec nisl.</p>
+                    <p>Fusce lacinia orci at nisi. Suspendisse at nisi nec diam pretium tincidunt. Ut vitae felis eu lectus ultrices varius. Aliquam lacus turpis, dapibus eget, tincidunt eu, lobortis et, magna. Integer pellentesque dignissim diam. </p>
+                    <p><a href="blog_post.html">24 Comments</a> | <a href="blog_post.html">Continue reading...</a>                </p>
+</div>
+                <div class="cleaner"></div>
+            </div>
+                
+            <div class="post_section">
+                    
+                
+            
+              
+              
+              	<div class="post_date">
+                	24<span>Oct</span>
+            	</div>
+                
+                <div class="post_content">
+                	<h2><a href="blog_post.html">Lorem ipsum dolor sit amet</a></h2>
+                	<strong>Author:</strong> Templatemo <strong>Category:</strong> <a href="#">HTML</a>, <a href="#">CSS</a>
+                
+                	<a href="http://www.templatemo.com/page/2" target="_parent"><img src="images/templatemo_image_02.jpg" alt="image" /></a>
+                
+                	<p>Validate <a href="http://validator.w3.org/check?uri=referer" rel="nofollow">XHTML</a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow">CSS</a>. Credits go to <a href="http://www.photovaco.com" target="_blank">free photos</a> for photos, <a href="http://www.smashingmagazine.com/2008/09/23/practika-a-free-icon-set/" target="_blank">Smashing Magazine</a> for icons and <a href="http://www.brusheezy.com" target="_blank">Nose-Meat</a> and <a href="http://www.brusheezy.com" target="_blank">FortuneGfx</a> for brushes. Phasellus cursus lobortis arcu. Donec scelerisque. Integer ultrices. Vivamus eu tortor. Phasellus tempus, justo et laoreet varius, odio mi ultrices libero, nec faucibus dui felis eu dui. Cras ac odio ac mi imperdiet sollicitudin.</p>
+               	  <a href="blog_post.html">58 Comments</a> | <a href="blog_post.html">Continue reading...</a>
+            
+            	</div>
+                <div class="cleaner"></div>
+            </div>
+        
+       	  </div>
+            
+          
+          <div id="templatemo_sidebar">
+            	
+                <h4>Categories</h4>
+                <ul class="templatemo_list">
+                    <li><a href="index.html">Praesent adipiscing</a></li>
+                    <li><a href="index.html">Duis sed justo</a></li>
+                    <li><a href="index.html">Mauris vulputate</a></li>
+                    <li><a href="index.html">Nam auctor</a></li>
+                    <li><a href="index.html">Aliquam quam</a></li>
+                </ul>
+                
+                <div class="cleaner_h40"></div>
+                
+                <h4>Friends</h4>
+                <ul class="templatemo_list">
+                    <li><a href="http://www.templatemo.com" target="_parent">Free CSS Templates</a></li>
+                    <li><a href="http://www.flashmo.com" target="_parent">Flash Templates</a></li>
+                    <li><a href="http://www.templatemo.com/page/1" target="_parent">Free Blog Themes</a></li>
+                    <li><a href="http://www.webdesignmo.com/blog" target="_parent">Web Design Blog</a></li>
+                    <li><a href="http://www.koflash.com" target="_parent">Flash Websites Gallery</a></li>
+                    <li><a href="#">Vestibulum laoreet</a></li>
+                    <li><a href="#">Nullam nec mi enim</a></li>
+                    <li><a href="#">Aliquam quam nulla</a></li>
+                </ul>
+                
+                <div id="ads">
+                	<a href="#"><img src="images/templatemo_200x100_banner.jpg" alt="banner 1" /></a>
+                    
+                    <a href="#"><img src="images/templatemo_200x100_banner.jpg" alt="banner 2" /></a>
+                </div>
+                
+            </div>
+        
+        	<div class="cleaner"></div>
+            
+        </div>
+        
+    </div>
+    
+    <div id="templatemo_main_bottom"></div>
+    
+</div>
+
+<div id="templatemo_footer">
+
+    Copyright © 2048 <a href="index.html">Your Company Name</a> | 
+    <a href="http://www.iwebsitetemplate.com" target="_parent">Website Templates</a> by <a href="http://www.templatemo.com" target="_parent">Free CSS Templates</a>
+    
+</div>
+
+<div align=center>This template  downloaded form <a href='http://all-free-download.com/free-website-templates/'>free website templates</a></div></body>
+</html>
